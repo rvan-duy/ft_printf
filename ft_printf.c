009683aa97e99989f2b095e59446df456304f2cb
ruben <rvan-duy@codam.nl>
2020-12-20 19:15:14 +0100

    tests

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,34 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/19 20:21:53 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/12/19 20:26:00 by rubenz        ########   odam.nl         */
+/*   Updated: 2020/12/20 18:53:22 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_printf(const char *, ...)
-{
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
+int ft_printf(const char * c, ...)
+{
+    va_list args;
+    va_start(args, c);
+
+    if (*c == '%')
+    {
+        int i = va_arg(args, int);
+        write(1, i, 2);
+    }
+    else
+    {
+        write()
+    }
+    
 }
 
+int main()
+{
+    printf("asd");
+    ft_printf("asd");
+    return 0;
+}
