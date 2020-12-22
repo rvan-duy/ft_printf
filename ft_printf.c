dcd280910db04b343db4790aa457a156bdb90051
ruben <rvan-duy@codam.nl>
2020-12-22 18:00:20 +0100

    rewrote code

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/19 20:21:53 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/12/22 16:21:51 by rubenz        ########   odam.nl         */
+/*   Updated: 2020/12/22 17:58:41 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	printline(const char *c)
-{
-	int i;
-
-	i = 0;
-	while (c[i])
-	{
-		if (c[i] == '%')
-			return (i);
-		write(1, c + i, 1);
-		i++;
-	}
-	return (-1);
-}
-
 int	ft_printf(const char *c, ...)
 {
-    	va_list args;
-    	int i;
+    va_list args;
+    int i;
 
+    i = 0;
 	va_start(args, c);
-    	i = printline(c);
-    	if (c[i] == '%')
-    	{
-        	i++;
-		if (c[i] == '%')
-			write (1, c + i, 1);
-    	}
-	return (0);
+    while (c[i])
+    {
+        if (c[i] == '%')
+        {
+            i++;
+            if (c[i] == '%')
+                write(1, c + i, 1);
+        }
+        else
+        {
+            write (1, c + i, 1);
+            i++;
+        }
+    }
+	return (1);
 }
 
 int main()
 {
-    	//printf("%");
 	printf("line%%\n");
-    	ft_printf("line%%\n");
-    	return 0;
+    ft_printf("line%%\n");
+    return 0;
 }
