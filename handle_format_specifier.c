b14ec90e9ceca8e841bad63ebc94eeb327460d80
ruben <rvan-duy@codam.nl>
2021-01-06 17:28:11 +0100

    note for next time

--- handle_format_specifier.c
+++ handle_format_specifier.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/06 17:19:11 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/01/06 17:19:23 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/06 17:27:58 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void        handle_format_specifier(const char *c, va_list args)
     parameters params;
     
     c++;
-    params = make_struct(c, args);
+    params = make_struct_format(c, args);
+    // check if it stops at the specifier, it shouldn't free the whole string. Unless I only give it the string with specifiers.
 }
