1c4464d596708c992051bbc9dc18a8c15d9c74d2
ruben <rvan-duy@codam.nl>
2021-01-14 17:36:43 +0100

    norm

--- specifiers/hexadecimal.c
+++ specifiers/hexadecimal.c
@@ -6,67 +6,67 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/14 11:27:49 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/01/14 17:17:18 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/14 17:35:33 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int     hex_len(unsigned int n)
+static int	hex_len(unsigned int n)
 {
-    int len;
-    
-    len = 0;
-    while (n > 0)
-    {
-        n /= 16;
-        len++;
-    }
-    return (len);
+	int len;
+
+	len = 0;
+	while (n > 0)
+	{
+		n /= 16;
+		len++;
+	}
+	return (len);
 }
 
-char    *itox(unsigned int n, char specifier)
+static char	*itox(unsigned int n, char specifier)
 {
-    char    *str;
-    char    *counters;
-    int     len;
+	char	*str;
+	char	*counters;
+	int		len;
 
-    if (!n)
-        return (ft_strdup("0"));
-    if (specifier == 'x')
-        counters = ft_strdup(HEXADECIMALS_LOWER);
-    if (specifier == 'X')
-        counters = ft_strdup(HEXADECIMALS_UPPER);
-    if (!counters)
-        return (NULL);
-    len = hex_len(n);
-    str = ft_calloc(len + 1, sizeof(char));
-    len -= 1;
-    while (n > 0)
-    {
-        str[len] = counters[n % 16];
-        n /= 16;
-        len--;
-    }
-    return (str);
+	if (!n)
+		return (ft_strdup("0"));
+	if (specifier == 'x')
+		counters = ft_strdup(HEXADECIMALS_LOWER);
+	if (specifier == 'X')
+		counters = ft_strdup(HEXADECIMALS_UPPER);
+	if (!counters)
+		return (NULL);
+	len = hex_len(n);
+	str = ft_calloc(len + 1, sizeof(char));
+	len -= 1;
+	while (n > 0)
+	{
+		str[len] = counters[n % 16];
+		n /= 16;
+		len--;
+	}
+	return (str);
 }
 
-char    *generate_x_string(parameters input, va_list args)
+char		*generate_x_string(parameters input, va_list args)
 {
-    char    *str;
-    char    padder;
-    int     decimal;
-    char    *tmp1;
-    char    *tmp2;
+	char	*str;
+	char	padder;
+	int		decimal;
+	char	*tmp1;
+	char	*tmp2;
 
-    padder = find_padder(input.flag_zero);
-    if (!input.width)
-        input.width = 1;
-    decimal = va_arg(args, unsigned int);
-    tmp1 = itox(decimal, input.specifier);
-    if (!tmp1)
-        return (NULL);
-    tmp2 = expand_str(tmp1, '0', input.precision, 0);
+	padder = find_padder(input.flag_zero);
+	if (!input.width)
+		input.width = 1;
+	decimal = va_arg(args, unsigned int);
+	tmp1 = itox(decimal, input.specifier);
+	if (!tmp1)
+		return (NULL);
+	tmp2 = expand_str(tmp1, '0', input.precision, 0);
 	free(tmp1);
 	if (!tmp2)
 		return (NULL);
--- specifiers/string.c
+++ specifiers/string.c
@@ -6,22 +6,22 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/12 18:33:59 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/01/13 16:12:55 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/14 17:33:27 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-char    *generate_s_string(parameters input, va_list args)
+char	*generate_s_string(parameters input, va_list args)
 {
-    char *str;
-    char *tmp;
+	char *str;
+	char *tmp;
 
-    if (!input.width)
-        input.width = 1;
-    tmp = va_arg(args, char*);
-    str = expand_str(tmp, ' ', input.width, input.flag_minus);
-    if (!str)
-        return (NULL);
-    return (str);
+	if (!input.width)
+		input.width = 1;
+	tmp = va_arg(args, char*);
+	str = expand_str(tmp, ' ', input.width, input.flag_minus);
+	if (!str)
+		return (NULL);
+	return (str);
 }
