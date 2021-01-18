1ca1ae59eb5593985ee982b8486a54cfdc5f4fe1
ruben <rvan-duy@codam.nl>
2021-01-19 00:47:45 +0100

    norm

--- utils.c
+++ utils.c
@@ -6,13 +6,13 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/12 23:06:11 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/01/17 12:23:12 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/19 00:46:28 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char    pf_padder_find(int flag_zero)
+char	pf_padder_find(int flag_zero)
 {
 	if (flag_zero)
 		return ('0');
