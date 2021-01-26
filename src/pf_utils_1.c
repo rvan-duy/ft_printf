79ed767fb01b5cad2fa4a9e63baad477c02aa6ea
ruben <rvan-duy@codam.nl>
2021-01-26 10:59:55 +0100

    split file

--- src/pf_utils_1.c
+++ src/pf_utils_1.c
@@ -6,12 +6,11 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/24 01:42:04 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/01/25 22:33:15 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/26 10:51:48 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 int			pf_specifier_len(const char *c, t_params *p)
 {
@@ -45,69 +44,9 @@ void		pf_flags_read(t_params *p)
 		p->flag_zero = 0;
 }
 
-static int pf_asterisk_read(t_params *p, va_list args, char type)
-{
-	int value;
-
-	value = va_arg(args, int);
-	p->str++;
-	if (value < 0 && type == WIDTH)
-	{
-		p->flag_minus = 1;
-		value *= -1;
-	}
-	if (value < 0 && type == PRECISION)
-		return (0);
-	return (value);
-}
-
-void		pf_width_read(t_params *p, va_list args)
-{
-	if (*p->str == '*')
-		p->width = pf_asterisk_read(p, args, WIDTH);
-	else
-		p->width = ft_atoi(p->str);
-	while (ft_isdigit(*p->str))
-		p->str++;
-	return ;
-}
-
-void		pf_precision_read(t_params *p, va_list args)
-{
-	if (*p->str == '.')
-		p->str++;
-	else
-		return ;
-	if (*p->str == '*')
-		p->precision = pf_asterisk_read(p, args, PRECISION);
-	else
-		p->precision = ft_atoi(p->str);
-	while (ft_isdigit(*p->str))
-		p->str++;
-}
-
 void		pf_specifier_read(t_params *p)
 {
 	p->specifier = *p->str;
 	p->str++;
 	return ;
-}
-
-char	*pf_strjoin(char const *s1, char *s2)
-{
-	char	*newstr;
-	int		len1;
-	int		len2;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	newstr = ft_calloc(len1 + len2 + 1, sizeof(char));
-	if (!newstr)
-		return (NULL);
-	ft_strlcpy(newstr, s1, len1 + 1);
-	ft_strlcat(newstr, s2, len1 + len2 + 1);
-	free(s2);
-	return (newstr);
 }
