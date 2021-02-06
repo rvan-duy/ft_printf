d5ff72c1e59dd22df7e8e70d3ae93ce635709d41
Ruben <ruben@Rubens-Mac-mini.local>
2021-02-06 18:56:29 +0100

    fixed u

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/04 10:29:44 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/02/06 17:47:54 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/02/06 18:35:01 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	ft_printf(const char *c, ...);
 
 int main()
 {
-    int d = -4825;
-    int ret1 = printf("(%5%)\n");
-    int ret2 = ft_printf("(%5%)\n");
+    unsigned int u = 0;
+    int ret1 = printf("(%.0u)\n", u);
+    int ret2 = ft_printf("(%0.0u)\n", u);
     printf("ret1: %d\nret2: %d\n", ret1, ret2);
 
     return 0;
--- src/specifiers/pf_string_u_create.c
+++ src/specifiers/pf_string_u_create.c
@@ -6,23 +6,61 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/25 18:34:10 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/02/05 13:04:22 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/02/06 18:55:19 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		pf_string_u_create(t_params *p, va_list args)
+static int	pf_utoa_intlen(unsigned int n)
+{
+	int		len;
+
+	len = 0;
+	while (n >= 10)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len + 1);
+}
+
+static char	*pf_utoa(t_params *p, unsigned int n)
+{
+	char	*newstr;
+	int		i;
+
+	if (n == 0 && p->precision == 0)
+		return (ft_strdup(""));
+	i = pf_utoa_intlen(n);
+	newstr = ft_calloc(sizeof(char), i + 1);
+	if (!newstr)
+		return (NULL);
+	i--;
+	if (n == 0)
+		newstr[i] = '0';
+	while (n > 0)
+	{
+		newstr[i] = 48 + (n % 10);
+		n = n / 10;
+		i--;
+	}
+	return (newstr);
+}
+
+int	pf_string_u_create(t_params *p, va_list args)
 {
 	char	*str;
 	char	padder;
 	int		ret;
 
 	padder = pf_padder_find(p->flag_zero);
-	str = ft_utoa(va_arg(args, unsigned int));
+	if (p->precision >= 0 && p->flag_zero)
+		padder = ' ';
+	str = pf_utoa(p, va_arg(args, unsigned int));
+	str = pf_string_expand(str, '0', p->precision, 0);
 	str = pf_string_expand(str, padder, p->width, p->flag_minus);
 	ret = ft_putstr_fd(str, 1);
 	free(str);
 	return (ret);
-	return (ft_putstr_fd(str, 1));
 }
